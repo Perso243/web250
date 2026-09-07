25655f168bd6928282cc43479f9adf1694355687
Eli <elijahtardiff@gmail.com>
2026-09-07 17:34:36 -0400

    Completed challenge 02 update

--- asgn02-inheritance/challenge-02-inheritance.php
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -1,27 +1,43 @@
 <?php
   class TTRPG { // tabletop role-playing games
-    var $name;
-    var $author;
-    var $dice = []; // dice types needed for game
-    var $playerMin = 4;
-    var $playerMax = 6;
+    public $name;
+    public $author;
+    public $dice = []; // dice types needed for game
+    protected $playerMin = 4;
+    protected $playerMax = 6;
 
-    function howManyPlayers() {
+    public function setPlayerMin($value) { // will only update if the new minimum is lower or equal to the current maximum
+      $this->playerMin = $value <= $this->playerMax ? $value : $this->playerMin;
+    }
+
+    public function getPlayerMin() {
+      return $this->playerMin;
+    }
+
+    public function setPlayerMax($value) { // will only update if the new minimum is lower or equal to the current maximum
+      $this->playerMax = $value >= $this->playerMin ? $value : $this->playerMax;
+    }
+
+    public function getPlayerMax() {
+      return $this->playerMax;
+    }
+
+    public function howManyPlayers() {
       return "{$this->name} can host between {$this->playerMin} and {$this->playerMax} players, plus a GM.";
     }
   }
 
   class D20 extends TTRPG { // D20 systems like D&D
-    var $dice = ["d4","d6","d8","d10","d12","d20"];
+    public $dice = ["d4","d6","d8","d10","d12","d20"];
   }
   class PBTA extends TTRPG { // Powered by the Apocalypse systems
-    var $dice = ["d6"];
-    var $playerMin = 3;
+    public $dice = ["d6"];
+    protected $playerMin = 3;
   }
 
   class NDNM extends TTRPG { // No Dice No Masters systems
-    var $playerMin = 1;
-    var $playerMax = 5;
+    protected $playerMin = 1;
+    protected $playerMax = 5;
 
     function howManyPlayers()
     {
@@ -44,5 +60,12 @@
   echo $dnd->howManyPlayers() . "<br>";
   echo $mh->howManyPlayers() . "<br>";
   echo $wh->howManyPlayers() . "<br>";
+  echo "<br>";
+
+  echo "Using new setter: <br>";
+  $mh->setPlayerMin(10);
+  echo "Set player minimum to 10, result: " . $mh->getPlayerMin() . "<br>";
+  $mh->setPlayerMin(5);
+  echo "Set player minimum to 5, result: " . $mh->getPlayerMin() . "<br>";
 
 ?>
